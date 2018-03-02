a496ba177a3ef5d8096d8e566cc9a5f225e44e61
tingxuelouwq <kevin_wangqi@126.co>
2018-03-02 21:41:41 +0800

    学习java8

---
+++ src/com/kevin/chap1/FilteringApples.java
@@ -0,0 +1,91 @@
+package com.kevin.chap1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * @类名: FilteringApples
+ * @包名：com.kevin.chap1
+ * @作者：kevin[wangqi2017@xinhua.org]
+ * @时间：2018/3/2 15:38
+ * @版本：1.0
+ * @描述：
+ */
+public class FilteringApples {
+
+    public static void main(String[] args) {
+        List<Apple> inventory = Arrays.asList(new Apple(80, "green"),
+                new Apple(155, "green"),
+                new Apple(120, "red"));
+
+
+        List<Apple> greenApples = filterApples(inventory, FilteringApples::isGreenApple);
+        System.out.println(greenApples);
+    }
+
+    public static List<Apple> filterGreenAples(List<Apple> inventory) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if ("green".equals(apple.getColor())) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public static List<Apple> filterHeavyApples(List<Apple> inventory) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (apple.getWeight() > 150) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public static boolean isGreenApple(Apple apple) {
+        return "green".equals(apple.getColor());
+    }
+
+    public static boolean isHeavyApple(Apple apple) {
+        return apple.getWeight() > 150;
+    }
+
+    public static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (p.test(apple)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public static class Apple {
+        private int weight = 0;
+        private String color = "";
+
+        public Apple(int weight, String color) {
+            this.weight = weight;
+            this.color = color;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+    }
+}
