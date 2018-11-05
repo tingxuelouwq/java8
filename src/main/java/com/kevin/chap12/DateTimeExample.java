8c327975351ed77ebfbf654cb2b09612301003ce
kevin <kevin_wangqi@126.com>
2018-11-05 18:57:08 +0800

    Temporal

--- src/main/java/com/kevin/chap12/DateTimeExample.java
+++ src/main/java/com/kevin/chap12/DateTimeExample.java
@@ -3,9 +3,13 @@ package com.kevin.chap12;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.*;
-import java.time.temporal.ChronoField;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.*;
 import java.util.Calendar;
 import java.util.Date;
+
+import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
+import static java.time.temporal.TemporalAdjusters.nextOrSame;
 
 /**
  * 类名: DateTimeExample<br/>
@@ -25,7 +29,8 @@ public class DateTimeExample {
 
     public static void main(String[] args) {
 //        useOldDate();
-        useLocalDate();
+//        useLocalDate();
+        useTemporalAdjuster();
     }
 
     private static void useOldDate() {
@@ -55,7 +60,7 @@ public class DateTimeExample {
 
         LocalTime time = LocalTime.of(13, 45, 20);  // 13:45:20
         int hour = time.getHour();  // 13
-        int minute =time.getMinute();   // 45
+        int minute = time.getMinute();   // 45
         int second = time.getSecond();  // 20
         System.out.println(time);
 
@@ -82,5 +87,88 @@ public class DateTimeExample {
         Duration d2 = Duration.between(instant, now);
         System.out.println(d1.getSeconds());
         System.out.println(d2.getSeconds());
+
+        Period tenDays = Period.between(LocalDate.of(2014, 3, 8),
+                LocalDate.of(2014, 3, 18));
+        System.out.println(tenDays);
+
+        Duration threeMinutes = Duration.ofMinutes(3);
+        Duration fiveMinutes = Duration.of(5, ChronoUnit.MINUTES);
+        Period fiveDays = Period.ofDays(5);
+        Period threeWeeks = Period.ofWeeks(3);
+        Period twoYearsSixMonthsOneDay = Period.of(2, 6, 1);
+    }
+
+    private static void useTemporalAdjuster() {
+        LocalDate date = LocalDate.of(2014, 3, 18);
+        date = date.with(nextOrSame(DayOfWeek.SUNDAY));
+        System.out.println(date);
+        date = date.with(lastDayOfMonth());
+        System.out.println(date);
+
+        date = date.with(new NextWorkingDay());
+        System.out.println(date);
+        date = date.with(nextOrSame(DayOfWeek.FRIDAY));
+        System.out.println(date);
+        date = date.with(new NextWorkingDay());
+        System.out.println(date);
+
+        date = date.with(nextOrSame(DayOfWeek.FRIDAY));
+        System.out.println(date);
+        // 使用Lambda表达式封装逻辑
+        date = date.with(temporal -> {
+            DayOfWeek dow = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
+            int dayToAdd = 1;
+            if (dow == DayOfWeek.FRIDAY) {
+                dayToAdd = 3;
+            }
+            if (dow == DayOfWeek.SATURDAY) {
+                dayToAdd = 2;
+            }
+            return temporal.plus(dayToAdd, ChronoUnit.DAYS);
+        });
+        System.out.println(date);
+
+        date = date.with(nextOrSame(DayOfWeek.FRIDAY));
+        System.out.println(date);
+        // 使用TemporalAdjusters.ofDateAdjuster静态工厂方法封装逻辑
+        TemporalAdjuster nextWorkingDay = TemporalAdjusters.ofDateAdjuster(
+                temporal -> {
+                    DayOfWeek dow = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
+                    int dayToAdd = 1;
+                    if (dow == DayOfWeek.FRIDAY) {
+                        dayToAdd = 3;
+                    }
+                    if (dow == DayOfWeek.SATURDAY) {
+                        dayToAdd = 2;
+                    }
+                    return temporal.plus(dayToAdd, ChronoUnit.DAYS);
+                }
+        );
+        date = date.with(nextWorkingDay);
+        System.out.println(date);
+    }
+
+    // 单独定义一个类封装逻辑
+    private static class NextWorkingDay implements TemporalAdjuster {
+        @Override
+        public Temporal adjustInto(Temporal temporal) {
+            // 读取当前日期
+            DayOfWeek dow = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
+            // 正常情况增加1天
+            int dayToAdd = 1;
+            if (dow == DayOfWeek.FRIDAY) {  // 如果当天是周五，增加3天
+                dayToAdd = 3;
+            } else if (dow == DayOfWeek.SATURDAY) { // 如果当天是周六，增加2天
+                dayToAdd = 2;
+            }
+            // 增加恰当的天数后，返回修改的日期
+            return temporal.plus(dayToAdd, ChronoUnit.DAYS);
+        }
+    }
+
+    private static void useDateFormatter() {
+        LocalDate date = LocalDate.of(2014, 3, 18);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     }
 }
